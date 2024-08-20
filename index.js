a50c8edc28da38fe197a7f4136a273505566917a
Luong2508 <tranvanluong032020@gmai.com>
2024-08-20 21:18:53 +0700

    add static file

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express')
 const app = express()
+
 require("dotenv").config();
 const port = process.env.PORT
-// const mongoose = require("mongoose")
-// mongoose.connect('mongodb://127.0.0.1:27017/products-test');
-// const Product = mongoose.model('Product', { 
-//     title: String,
-//     price: Number,
-//     price: Number,
-//     thumbnail: String
-//  });
+
 app.set("views","./views")
 app.set("view engine","pug")
+
+app.use(express.static('public'))
 
 const route = require("./routes/client/index.route.js")
 route(app);
